3017356bfd0781013f5f8d949b71a0b0fa5b0026
Denis Kotov <DAKotov@ptc.local>
2016-08-08 10:38:04 +0300

    Fix is_bst() and some other functions

--- Python/bst.py
+++ Python/bst.py
@@ -40,6 +40,9 @@ class Tree:
 	def __lt__(self, other):
 		return self.data < other.data
 
+	def __le__(self, other):
+		return self.data <= other.data
+
 	def __str__(self):
 		return str(self.data)
 
@@ -83,14 +86,15 @@ def is_bst(tree):
 	if not tree:
 		return True
 
-	left_ok = not tree.left or tree.left < tree
-	right_ok = not tree.right or tree < tree.right
-
-	return 	left_ok and right_ok and \
-		is_bst(tree.left) and is_bst(tree.right)
+	left_ok = (not tree.left) or \
+			(tree.left and max_node(tree.left) <= tree)
+	right_ok = (not tree.right) or \
+			(tree.right and tree < min_node(tree.right))
+	return left_ok and right_ok and \
+		is_bst(tree.left) and is_bst(tree.right) 
 
 def min_node_recursce(bst):
-	assert is_bst(bst) and bst is not None
+	assert is_bst(bst)
 
 	if not bst.left:
 		return bst
@@ -98,27 +102,52 @@ def min_node_recursce(bst):
 	return min_node_recursce(bst.left)
 
 def min_node(bst):
-	assert is_bst(bst) and bst is not None
-
-	while bst.left:
-		bst = bst.left
+	if bst:
+		if bst.left and bst.right:
+			return min(min_node(bst.left), min_node(bst.right)) 
+		elif bst.left:
+			return bst.left
+		elif bst.right:
+			return bst.right
+		else:
+			return bst
 	return bst
 
 def max_node(bst):
-	assert is_bst(bst) and bst is not None
-
-	while bst.right:
-		bst = bst.right
+	if bst:
+		if bst.left and bst.right:
+			return max(min_node(bst.left), min_node(bst.right)) 
+		elif bst.left:
+			return bst.left
+		elif bst.right:
+			return bst.right
+		else:
+			return bst
 	return bst
 
 def next_node(bst):
+	assert is_bst(bst)
+
 	if bst.right:
-		min_node(bst.right)
+		return min_node(bst.right)
+
+	if bst.parent.left == bst:
+		return bst.parent
+
+	while bst.parent and bst.parent.right == bst:
+		bst = bst.parent
+
 	return bst.parent
 
 def prev_node(bst):
 	if bst.left:
-		max_node(bst.right)
+		return max_node(bst.left)
+
+	if bst.parent.right == bst:
+		return bst.parent
+
+	if bst.parent.right and bst.parent.left == bst:
+		bst = bst.parent
 	return bst.parent
 
 begin = min_node
@@ -129,24 +158,25 @@ def end(bst):
 def iterator(bst):
 	it = begin(bst)
 	while it != end(bst):
+		print("it = ", bst)
 		yield it
 		it = next_node(it)
 
 def lower_bound(bst, x):
-	#assert is_bst(bst)
-
+	assert is_bst(bst)
+	
 	if bst:
 		if x < bst.data:
 			res = lower_bound(bst.left, x)
 			if res:
 				bst = res
 		elif bst.data < x:
-			return lower_bound(bst.data, x)
+			return lower_bound(bst.right, x)
 	return bst
 
 
 def insert(tree, x):
-	#assert is_bst(tree)
+	assert is_bst(tree)
 
 	if not tree:
 		return Tree(x)
@@ -156,12 +186,14 @@ def insert(tree, x):
 			return insert(tree.left, x)
 		else:
 			tree.left = Tree(x)
+			tree.left.parent = tree
 			return tree.left
 	elif tree.data < x:
 		if tree.right:
 			return insert(tree.right, x)
 		else:
 			tree.right = Tree(x)
+			tree.right.parent = tree
 			return tree.right
 
 	return tree
@@ -173,9 +205,10 @@ def dfs_nodes(tree):
 		yield from dfs_nodes(tree.right)
 
 def remove(tree, x):
-	place = find(tree, x)
-	rotate_left(rotate_left(place)()
-	place = find(tree, x)
+	#place = find(tree, x)
+	#rotate_left(rotate_left(place)()
+	#place = find(tree, x)
+	pass
 
 def rotate_left(node):
 	if node and node.right:
@@ -184,7 +217,6 @@ def rotate_left(node):
 		node.right = node_left
 		new_root.left = node
 		return node
-
 
 def create_tree_1():
 	return Tree(10, 
@@ -206,7 +238,6 @@ def create_tree_1():
 			)
 		)
 
-
 def create_tree_2():
 	return Tree(8, 
 		Tree(3, 
@@ -218,21 +249,34 @@ def create_tree_2():
 			), 
 		Tree(10,
 			right=Tree(14,
-				right=Tree(13)
+						Tree(13)
 				)
 			)
 		)
 
+def create_tree_3():
+	return Tree(3, 
+			Tree(2),
+			Tree(5,
+				Tree(1),
+				Tree(7))
+			)
 
 def main():
 	t = create_tree_1()
+	t2 = create_tree_2()
+	t3 = create_tree_3()
 
-	print(lower_bound(t, 10))
+	#print("min_node() is "+str(min_node(t3.right)))
+	print("Good tree1 is_bst() is "+str(is_bst(t)))
+	print("Good tree2 is_bst() is "+str(is_bst(t)))
+	print("Bad tree3 is_bst() is "+str(is_bst(t3)))
+	print(lower_bound(t2, 10))
 
 	for i in range(25):
 		print("Insert "+str(i))
-		insert(t, i)
-		print(", ".join(map(str, dfs_nodes(t))))
+		insert(t2, i)
+		print(", ".join(map(str, dfs_nodes(t2))))
 	t.draw()
 
 if __name__ == '__main__':
